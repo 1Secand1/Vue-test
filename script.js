fbc7f6bb00ca04e01fe1597a49ffb73235c62599
Lisand <boevladik@gmail.com>
2023-08-11 23:02:34 +0300

    v-for

--- script.js
+++ script.js
@@ -2,19 +2,33 @@ const Counter = {
   data() {
     return {
       referencePassword: 1234,
-      password: undefined,
-      isActive: true,
+      password: 1234,
+      valueStatus: false,
+      lessonsActive: [],
+      lessons: ["Математика", "Русский", "Физика", "Абобалогия"],
     };
   },
   computed: {
     verification: function () {
       return {
-        "login__input login__input--accept ": this.referencePassword == this.password,
-        "login__input login__input--no-accept ": this.referencePassword != this.password,
+        "login__input login__input--accept ":
+          this.referencePassword == this.password,
+        "login__input login__input--no-accept ":
+          this.referencePassword != this.password,
       };
     },
   },
-  methods: {},
+  methods: {
+    check() {
+      this.valueStatus = +this.password === +this.referencePassword;
+    },
+    listOutput() {
+      this.lessons.forEach((e) => {
+        console.log(e);
+        this.lessonsActive.push(e);
+      });
+    },
+  },
 };
 
 Vue.createApp(Counter).mount("#main");
